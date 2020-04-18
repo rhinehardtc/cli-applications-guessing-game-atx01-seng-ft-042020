4292e009346b9a6754a82eaba153b18e92c8e6f2
Cameron Rhinehardt <rhinehardtc@gmail.com>
2020-04-18 01:24:01 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,10 +2,13 @@ require 'pry'
 
 def run_guessing_game
   #binding.pry
+  message = puts 'guess a number from 1 to 6'
   num = rand(6) + 1
   input = gets.chomp
+  
+  message
   num
-  puts 'guess a number from 1 to 6'
+  
   input
   if input == num
     puts "You guessed the correct number!"
