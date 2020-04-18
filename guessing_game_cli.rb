9c53233fd004655f7dad61726a474265cc28c4dd
Cameron Rhinehardt <rhinehardtc@gmail.com>
2020-04-18 01:31:31 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,15 +4,15 @@ def run_guessing_game
   
   message = puts 'guess a number from 1 to 6'
   num = rand(6) + 1
-  input = gets.chomp.to_i
+  input = gets.chomp
   #binding.pry
   message
   num
   
   input
-  if input == num
+  if input.to_i == num
     puts "You guessed the correct number!"
-  elsif input != num
+  elsif input.to_i != num
     puts "Sorry! The computer guessed #{num}."
   elsif input == "exit"
     puts "Goodbye!"
